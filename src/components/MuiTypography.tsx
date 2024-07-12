6ce51a549f4d3d12be0296c5d4ae0dd364cda51a
MOJAHED.ALO.HABIB <mojahed.alo.habib@gmail.com>
2024-07-12 13:16:47 +0800

    first commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>h1 Heading</Typography>
+        <Typography variant='h2'>h2 Heading</Typography>
+        <Typography variant='h3'>h3 Heading</Typography>
+        <Typography variant='h4'>h4 Heading</Typography>
+        <Typography variant='h5'>h5 Heading</Typography> 
+        <Typography variant='h6'>h6 Heading</Typography> 
+        <Typography variant='subtitle1'>Subtitle 1</Typography> 
+        <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, porro.</Typography> 
+        <Typography variant='body2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam, voluptas.</Typography> 
+    </div>
+  )
+}
+
+export default MuiTypography
